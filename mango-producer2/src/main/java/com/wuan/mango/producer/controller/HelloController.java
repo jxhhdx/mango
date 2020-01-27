54d95a0acbc3f01322eb8c83086f3e9c43b8f7de
gaoxiang <igaoxian@foxmail.com>
2020-01-27 15:36:41 +0800

    服务提供者

---
+++ mango-producer2/src/main/java/com/wuan/mango/producer/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.wuan.mango.producer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Notes
+ * @author gaoxiang
+ * @date 2020/01/27
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello Mango2 !";
+    }
+}
